7b63ae0cd2fecee5fd2ea200bd4c2e2898cd69a4
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-06-17 13:45:20 +0200

    Kreiranje Csharp datoteke te stavaljanje  CsharpVjezba na github

---
+++ CsharpVjezba/Csharp varijable.cs
@@ -0,0 +1,56 @@
+﻿// ovo je komentar i nece se izvesti
+int f = 4;
+
+int varijabla = 4;
+
+
+
+// deklaracija varijble
+int i;
+
+// dodjeljivanje vrijednosti
+i = 4;
+
+//korištenej varijable
+Console.WriteLine(i);
+
+string nizZnakova = "Edunova";
+
+bool logickaVrijednost = true;
+
+Console.WriteLine(logickaVrijednost);
+
+float decimalniBroj = 3.14f;
+
+double decimlaniBrojVeci = 3.14;
+
+decimal db = 3.14m;
+
+Console.WriteLine("decimalni broj: {0}\ndb: {1}",
+    decimalniBroj,db);
+
+Console.WriteLine(int.MinValue);
+Console.WriteLine(int.MaxValue);
+
+int b = int.MaxValue;
+    b = b + 1;
+
+Console.WriteLine(b);
+
+// zadatak: 1
+// deklarirajte 4 varijable različitih tipova podataka
+// svakom od njih dodjelite vrijednost po zelji, sve ih od jednom ispisite jednog pored drugog
+
+string s = "Webprogramiranje";
+bool b2 = false;
+decimal db2 = 2.47m;
+float f2 = 1.56f;
+
+Console.WriteLine("{0},{1},{2},{3}",s,b2,db2,f2);
+
+// tips & triks sa varijablama
+
+int a, v, c = 3;
+bool istina = w == 3;
+
+
