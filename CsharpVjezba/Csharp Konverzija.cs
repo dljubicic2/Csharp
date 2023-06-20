3409f7b5a5e7c64ae770cfae1c2b85a1a9fe2343
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-06-20 21:02:24 +0200

    Dodavanje vjezbe Csharp na github

---
+++ CsharpVjezba/Csharp Konverzija.cs
@@ -0,0 +1,29 @@
+﻿// implicitna konverzija
+float f = 7f;
+
+double d = f;
+
+// explicitna konverzija
+f = float.Parse(d. ToString());
+
+int a = 3;
+int d2 = 4;
+
+float rez = a / d2;
+
+Console.WriteLine(rez);
+
+// cast
+int a2 = 3;
+int b2 = 4;
+
+float rez2 = a2 / (float)b2;
+Console.WriteLine(rez2);
+
+int broj = 15;
+
+// koja je prva znamenka broja
+Console.WriteLine(broj/10);
+
+
+
