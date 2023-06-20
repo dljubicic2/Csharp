d7cd5547e7b3c8eaba1cb01494e9455353657fdd
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-06-20 21:01:34 +0200

    Create Csharp ulaz i izlaz.cs

---
+++ CsharpVjezba/Csharp ulaz i izlaz.cs
@@ -0,0 +1,24 @@
+﻿// Ulaz
+Console.Write("Unesite broj: ");
+int broj = int.Parse(Console.ReadLine());
+
+//Alogritam
+broj = broj + 7;
+
+// Izlaz
+Console.WriteLine(broj);
+
+// program unosi 2 decimalna broja 
+// program ispisuje njihov zbroj
+
+Console.WriteLine("Unesi prvi broj: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Unesi drugi broj: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double rez = b1 + b2;
+Console.WriteLine(rez);
+
+
+
+
