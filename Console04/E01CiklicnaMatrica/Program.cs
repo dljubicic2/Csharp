93e52cc05f23d280cc54daf6d1e6a22156609bce
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-07-07 17:31:49 +0200

    Ciklicna Matrica zadaca

---
+++ Console04/E01CiklicnaMatrica/Program.cs
@@ -0,0 +1,148 @@
+﻿// 1. Korak
+int redaka, stupaca;
+
+Console.Write("Unesite broj redaka");
+redaka=int.Parse(Console.ReadLine());
+
+Console.Write("Unesite broj stupaca");
+stupaca=int.Parse(Console.ReadLine());
+
+int[,] matrica = new int[redaka, stupaca];  // Tablica je napravljena
+
+// 2. Korak
+int a = 1; // Brojac
+for(int i =1; i<=stupaca; i++)
+{
+    matrica[redaka - 1, stupaca - i] = a++;
+}
+
+for(int i =0; i<redaka; i++)  // Provjera
+{
+    for(int j =0; j<stupaca; j++)
+    {
+        Console.Write(matrica[i,j]+"  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("********************************");
+
+// 3. Korak
+for(int i = redaka -2; i>0; i--)
+{
+    matrica[i,0] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("*************************************");
+
+// 4. Korak
+for(int i =1; i<stupaca; i++)
+{
+    matrica[0,i] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("*************************");
+
+// 5. Korak
+for(int i =1; i<=redaka-2; i++)
+{
+    matrica[i, 4] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    { 
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("*************************************");
+
+// 6. Korak
+for(int i =3; i>=stupaca-4; i--)
+{
+    matrica[3,i] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("******************************************");
+
+// 7. Korak
+for(int i =2; i>=redaka-4; i--)
+{
+    matrica[i, 1] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("********************************************");
+
+// 8. Korak
+for(int i=2; i<=stupaca-2; i++)
+{
+    matrica[1,i] = a++;
+}
+
+for (int i = 0; i < redaka; i++)  // Provjera
+{
+    for (int j = 0; j < stupaca; j++)
+    {
+        Console.Write(matrica[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("**************************************************");
+
+// 9. Korak
+for(int i =3; i>=stupaca-3; i--)
+{
+    matrica[2,i] = a++;
+}
+
+string ispis;
+
+for(int i =0; i<redaka; i++)
+{
+    for(int j=0; j<stupaca; j++)
+    {
+        ispis="  " + matrica[i,j];
+        Console.Write(ispis[^3..]);
+    }
+    Console.WriteLine();
+}
---
+++ Console04/E05CiklicnaMatrica/Program.cs
@@ -0,0 +1,155 @@
+﻿// Korak: 1
+int Redaka, Stupaca;
+Console.Write("Unesite broj stupaca");
+Stupaca=int.Parse(Console.ReadLine());
+Console.Write("Unesite broj redaka");
+Redaka=int.Parse(Console.ReadLine());
+int[,] matrica = new int[Redaka,Stupaca];
+
+for(int i=0; i < Stupaca; i++)
+{
+    for(int j=0; j < Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 2
+int a = 1;
+for ( int i =1; i<=Stupaca; i++)
+{
+    matrica[Redaka-1, Stupaca -i] = a++;
+}
+
+for (int i =0; i<Stupaca ; i++)  // Provjera
+{
+    for (int j=0; j<Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 3
+for(int i =Redaka-2;i>=0; i--)
+{
+    matrica[i,0] = a++;
+}
+
+for (int i =0; Stupaca>0; i++)  // Provjera
+{
+    for(int j =0; j < Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 4
+for (int i =1; i<Stupaca; i++)
+{
+    matrica[0,1] = a++;
+}
+
+for(int i =0; i<Redaka; i++)  // Provjera
+{
+    for(int j =0; j < Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 5
+for(int i =1; i <=Redaka -2; i++)
+{
+    matrica[i, 4] = a++;
+}
+
+for(int i =0; i<Stupaca; i++)  // Provjera
+{
+    for(int j =0; j<Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 6
+for(int i = 3; i >= Stupaca - 4; i--)
+{
+    matrica[3,1]= a++;
+}
+
+for(int i =0; i< Stupaca ; i++)  // Provjera
+{
+    for(int j =0; j<Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 7
+for (int i = 2; i>= Redaka - 4; i--)
+{
+    matrica[i,1]= a++;
+}
+
+for (int i = 0; i< Stupaca ; i++)  // Provjera
+{
+    for(int j =0; j<Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak: 8
+for (int i =2; i<=Stupaca-2; i++)
+{
+    matrica[1,i]= a++;
+}
+
+for(int i =0; i<Stupaca ; i++)  // Provjera
+{
+    for(int j=0; j<Redaka; j++)
+    {
+        Console.Write(matrica[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***********************");
+
+// Korak:9
+string ispis;
+for(int i =3; i>=Stupaca-3; i--)
+{
+    matrica[2,i]= a++;
+}
+
+for(int i =0; i<Stupaca; i++)
+{
+    for(int j=0; j<Redaka; j++)
+    {
+        ispis = " " + matrica[i,j];
+        Console.Write(ispis[^3..]);
+    }
+    Console.WriteLine();
+}
