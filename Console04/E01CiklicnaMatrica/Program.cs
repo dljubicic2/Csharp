42e9206bcdd65719bdd9d40e7f6bf619cbef78a4
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-07-10 21:49:22 +0200

    Update ciklicne matrice zadaca

--- Console04/E01CiklicnaMatrica/Program.cs
+++ Console04/E01CiklicnaMatrica/Program.cs
@@ -1,148 +1,70 @@
 ﻿// 1. Korak
-int redaka, stupaca;
-
-Console.Write("Unesite broj redaka");
-redaka=int.Parse(Console.ReadLine());
-
-Console.Write("Unesite broj stupaca");
-stupaca=int.Parse(Console.ReadLine());
-
-int[,] matrica = new int[redaka, stupaca];  // Tablica je napravljena
+int redaka = 5;
+int stupaca = 5;
+int[,] matrica = new int[redaka, stupaca]; // Kreirana je tablica
 
 // 2. Korak
-int a = 1; // Brojac
-for(int i =1; i<=stupaca; i++)
+int b = 1;
+for (int i = 1; i <= stupaca; i++)
 {
-    matrica[redaka - 1, stupaca - i] = a++;
+    matrica[redaka - 1, stupaca - i] = b++;
 }
 
-for(int i =0; i<redaka; i++)  // Provjera
+//3. Korak
+for (int i = redaka - 2; i >= 0; i--)
 {
-    for(int j =0; j<stupaca; j++)
-    {
-        Console.Write(matrica[i,j]+"  ");
-    }
-    Console.WriteLine();
+    matrica[i, 0] = b++;
 }
 
-Console.WriteLine("********************************");
+//4. Korak
+for (int i = 1; i <= stupaca - 1; i++)
 
-// 3. Korak
-for(int i = redaka -2; i>0; i--)
 {
-    matrica[i,0] = a++;
+    matrica[0, i] = b++;
 }
 
-for (int i = 0; i < redaka; i++)  // Provjera
+//5. Korak
+for (int i = 1; i <= stupaca - 2; i++)
+{
+    matrica[i, stupaca - 1] = b++;
+}
+
+//6. Korak
+for (int i = 3; i >= stupaca - 4; i--)
+{
+    matrica[3, i] = b++;
+}
+
+//7. Korak
+for (int i = 2; i >= redaka - 4; i--)
+{
+    matrica[i, 1] = b++;
+}
+
+//8. Korak
+for (int i = 2; i <= stupaca - 2; i++)
+{
+    matrica[1, i] = b++;
+}
+
+//9. Korak
+for (int i = 2; i <= stupaca - 3; i++)
+{
+    matrica[i, stupaca - 2] = b++;
+}
+
+//10. Korak
+string razmak;
+for (int i = 3; i < stupaca - 1; i++)
+{
+    matrica[redaka - 3, stupaca - i] = b++;
+}
+for (int i = 0; i < redaka; i++)  // Ispis
 {
     for (int j = 0; j < stupaca; j++)
     {
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("*************************************");
-
-// 4. Korak
-for(int i =1; i<stupaca; i++)
-{
-    matrica[0,i] = a++;
-}
-
-for (int i = 0; i < redaka; i++)  // Provjera
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("*************************");
-
-// 5. Korak
-for(int i =1; i<=redaka-2; i++)
-{
-    matrica[i, 4] = a++;
-}
-
-for (int i = 0; i < redaka; i++)  // Provjera
-{
-    for (int j = 0; j < stupaca; j++)
-    { 
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("*************************************");
-
-// 6. Korak
-for(int i =3; i>=stupaca-4; i--)
-{
-    matrica[3,i] = a++;
-}
-
-for (int i = 0; i < redaka; i++)  // Provjera
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("******************************************");
-
-// 7. Korak
-for(int i =2; i>=redaka-4; i--)
-{
-    matrica[i, 1] = a++;
-}
-
-for (int i = 0; i < redaka; i++)  // Provjera
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("********************************************");
-
-// 8. Korak
-for(int i=2; i<=stupaca-2; i++)
-{
-    matrica[1,i] = a++;
-}
-
-for (int i = 0; i < redaka; i++)  // Provjera
-{
-    for (int j = 0; j < stupaca; j++)
-    {
-        Console.Write(matrica[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("**************************************************");
-
-// 9. Korak
-for(int i =3; i>=stupaca-3; i--)
-{
-    matrica[2,i] = a++;
-}
-
-string ispis;
-
-for(int i =0; i<redaka; i++)
-{
-    for(int j=0; j<stupaca; j++)
-    {
-        ispis="  " + matrica[i,j];
-        Console.Write(ispis[^3..]);
+        razmak = "   " + matrica[i, j];
+        Console.Write(razmak[^4..]);
     }
     Console.WriteLine();
 }
