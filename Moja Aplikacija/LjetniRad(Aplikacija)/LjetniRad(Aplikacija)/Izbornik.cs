5fd85e5e238dbb52a3768db388d2086d00a8d873
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-07-16 16:37:34 +0200

    Moja aplikacija (nije dovrseno!)

---
+++ Moja Aplikacija/LjetniRad(Aplikacija)/LjetniRad(Aplikacija)/Izbornik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LjetniRad_Aplikacija_
+{
+    internal class Izbornik
+    {
+        public Izbornik() 
+        {
+            PozdravnaPoruka();
+            PrikaziIbornik();
+        }
+
+        private void PrikaziIbornik()
+        {
+            Console.WriteLine("Glavni izbornik");
+            Console.WriteLine("1. Upiti");
+            Console.WriteLine("2. Osobe");
+            Console.WriteLine("3. Oglasi");
+            Console.WriteLine("4. Vozila");
+        }
+
+        private void PozdravnaPoruka()
+        {
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("*** Edunova Console APP AUTO OGLASNIK ***");
+            Console.WriteLine("*****************************************");
+        }
+    }
+}
+
