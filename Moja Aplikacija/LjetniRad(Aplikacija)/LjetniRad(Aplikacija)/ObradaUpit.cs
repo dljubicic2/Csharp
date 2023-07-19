3c52405a31f983abaabb170c05641f7286cfd4cd
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-07-19 14:58:06 +0200

    Moja aplikacija (nije dovrseno!)

--- Moja Aplikacija/LjetniRad(Aplikacija)/LjetniRad(Aplikacija)/ObradaUpit.cs
+++ Moja Aplikacija/LjetniRad(Aplikacija)/LjetniRad(Aplikacija)/ObradaUpit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,15 @@ namespace LjetniRad_Aplikacija_
 {
     internal class ObradaUpit
     {
-        public List<Upit> Upiti {get;}
+        public List<Upit> Upiti { get; }
 
-        public ObradaUpit() 
+        public ObradaUpit()
         {
             Upiti = new List<Upit>();
-            
-            
-            
+            TestniPodaci();
+
+
+
         }
         public void PrikaziIzbornik()
         {
@@ -25,33 +27,61 @@ namespace LjetniRad_Aplikacija_
             Console.WriteLine("3. Promjena postojeceg upita");
             Console.WriteLine("4. Brisanje upita");
             Console.WriteLine("5. Povratak na glavni izbornik");
-            switch(Pomocno.UcitajBrojRaspon("Odaberite stavku izbornika upita: ",
-                "Odabir mora biti 1-5", 1,5))
-                {
+            switch (Pomocno.UcitajBrojRaspon("Odaberite stavku izbornika upita: ",
+                "Odabir mora biti 1-5", 1, 5))
+            {
                 case 1:
                     PrikaziUpite();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    UnosNovogUpita();
                     PrikaziIzbornik();
                     break;
                 case 5:
                     Console.WriteLine("Gotov rad s upitima");
                     break;
-                    
+
 
             }
+        }
+
+        private void UnosNovogUpita()
+        {
+            var s = new Upit
+            {
+                Sifra = Pomocno.UcitajCijeliBroj("Unesite sifru upita: ",
+                "Unos mora biti pozitivni cijeli broj"),
+                Oglas = Pomocno.UcitajCijeliBroj("Unesite broj oglasa: ",
+                "Unos obavezan"),
+                Pitanje = Pomocno.UcitajString("Unesi pitanje: ",
+                "Unos obavezan"),
+                VrijemeUpita = Pomocno.UcitajCijeliBroj("Unesi trajanje u satima: ",
+                "Unos mora biti pozitivan broj"),
+                Osoba = Pomocno.UcitajCijeliBroj("Unesi broj osobe: ",
+                "Unos obavezan")
+            };
+
         }
 
         private void PrikaziUpite()
         {
-            foreach(Upit upit in Upiti)
+            foreach (Upit upit in Upiti)
             {
                 Console.WriteLine(upit.Pitanje);
             }
+
         }
+        private void TestniPodaci()
+        {
+            Upiti.Add(new Upit { Pitanje = "Koliko je prešao?" });
+
+        }
+
+
+
+
+
     }
-    
-    
-        
-        
-    
 }
 
--- Moja Aplikacija/LjetniRad(Aplikacija)/LjetniRad(Aplikacija)/Pomocno.cs
+++ Moja Aplikacija/LjetniRad(Aplikacija)/LjetniRad(Aplikacija)/Pomocno.cs
@@ -31,6 +31,43 @@ namespace LjetniRad_Aplikacija_
             }
 
         }
+
+        internal static int UcitajCijeliBroj(string poruka, string greska)
+        {
+            int b;
+            while (true)
+            {
+                Console.Write(poruka);
+                try
+                {
+                    b=int.Parse(Console.ReadLine());
+                    if (b > 0)
+                    {
+                        return b;
+                    }
+                    Console.WriteLine(greska);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+        }
+
+        internal static string UcitajString(string poruka, string greska)
+        {
+            string s = " ";
+            while(true)
+            {
+                Console.Write(poruka);
+                s = Console.ReadLine();
+                if(s!=null && s.Trim().Length> 0)
+                {
+                    return s;
+                }
+                Console.WriteLine(greska);
+            }
+        }
     }
 
 }
