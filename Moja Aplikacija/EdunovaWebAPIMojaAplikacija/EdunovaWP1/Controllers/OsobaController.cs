36b5d8ce43041dd4ef202ca649d989f418d0c4f3
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-09-06 14:35:58 +0200

    Get, Post, Put i Delete na moj aplikaciji

---
+++ Moja Aplikacija/EdunovaWebAPIMojaAplikacija/EdunovaWP1/Controllers/OsobaController.cs
@@ -0,0 +1,33 @@
+﻿using EdunovaWP1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdunovaWP1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OsobaController : ControllerBase
+    {
+        [HttpGet]
+
+        [HttpPost]
+        public IActionResult Post(Osoba osoba)
+        {
+            return Created("/api/v1/osoba", osoba);
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(Osoba osoba)
+        {
+            return StatusCode(StatusCodes.Status200OK, osoba);
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            return StatusCode(StatusCodes.Status200OK, "{\"obrisano\":true}");
+        }
+    }
+}
---
+++ Moja Aplikacija/EdunovaWebAPIMojaAplikacija/EdunovaWP1/Models/Osoba.cs
@@ -0,0 +1,10 @@
+﻿namespace EdunovaWP1.Models
+{
+    public class Osoba : Entitet
+    {
+        public string? Nadimak { get; set; }
+        public string? Email { get; set; }
+        public string? Lozinka { get; set; }
+        public int BrojTelefona { get; set; }
+    }
+}
