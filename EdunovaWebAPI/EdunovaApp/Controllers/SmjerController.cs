90a342e4885af0ec1e261c540a0ca0092c203752
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-09-11 11:49:48 +0200

    Spajanje baze podataka, Get, Post, Put i Delete na našoj zajedničkoj aplikaciji

--- EdunovaWebAPI/EdunovaApp/Controllers/SmjerController.cs
+++ EdunovaWebAPI/EdunovaApp/Controllers/SmjerController.cs
@@ -1,6 +1,7 @@
 ﻿using EdunovaApp.Data;
 using EdunovaApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace EdunovaApp.Controllers
 {
@@ -18,18 +19,54 @@ namespace EdunovaApp.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+           // Kontrola u koliko upit nije dobar
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            return new JsonResult(_context.Smjer.ToList());
+            try
+            {
+                var smjerovi = _context.Smjer.ToList();
+                if(smjerovi==null || smjerovi.Count == 0)
+                {
+                    return new EmptyResult();
+                }
+
+                return new JsonResult(_context.Smjer.ToList());
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, 
+                                                               ex.Message);
+            }
+
+            
         }
 
         [HttpPost]
         public IActionResult Post(Smjer smjer)
         {
-            _context.Smjer.Add(smjer);
-            _context.SaveChanges();
+           
 
-            // Dodavanje u bazu
-            return Created("/api/v1/Smjer", smjer); //201
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                _context.Smjer.Add(smjer);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, smjer);
+            }catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                                              ex.Message);
+            }
+
+
+            
         }
 
         [HttpPut]
@@ -37,8 +74,34 @@ namespace EdunovaApp.Controllers
         public IActionResult Put(Smjer smjer, int sifra)
         {
             // Promjena u bazi
+            if (sifra <= 0 || smjer==null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var smjerBaza = _context.Smjer.Find(sifra);
+                if (smjerBaza == null)
+                {
+                    return BadRequest();
+                }
+                smjerBaza.Naziv = smjer.Naziv;
+                smjerBaza.Trajanje = smjer.Trajanje;
+                smjerBaza.Cijena = smjer.Cijena;
+                smjerBaza.Upisnina=smjer.Upisnina;
+                smjerBaza.Verificiran = smjer.Verificiran;
 
-            return StatusCode(StatusCodes.Status200OK, smjer);
+                _context.Smjer.Update(smjerBaza);
+                _context.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, smjerBaza);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                                             ex.Message);
+            }
+            
         }
 
         [HttpDelete]
@@ -46,9 +109,42 @@ namespace EdunovaApp.Controllers
         [Produces("application/json")]
         public IActionResult Delete(int sifra)
         {
-            // Brisanje u bazi
-            return StatusCode(StatusCodes.Status200OK, "{\"obrisano\":true}");
+            if (sifra <= 0)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var smjerBaza = _context.Smjer.Find(sifra);
+                if (smjerBaza == null)
+                {
+                    return BadRequest();
+                }
 
+                _context.Smjer.Remove(smjerBaza);
+                _context.SaveChanges();
+
+                return new JsonResult("{\"poruka\":\"Obrisano\"}");
+
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    SqlException sqle = (SqlException)ex;
+                    return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                   sqle);
+                }
+                catch (Exception e)
+                {
+
+
+                }
+
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                                    ex.Message);
+
+            }
         }
     }
 }
